5af50f279b355cf3416469deafb7844f3ec12c0a
Trigg Summs <stsumms@gmail.com>
2021-04-14 12:48:28 -0500

    Testing backup computer on this repo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+        <h1>Hello World!, I am testing a different machine(*backup pc) to push code</h1>
       </div>
     );
   }
